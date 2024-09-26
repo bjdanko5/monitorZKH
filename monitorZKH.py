a9537d8e9d3511cc206c0b52191f31f865bc5f23
Boris Zdanko <bjdanko5@gmail.com>
2024-09-26 22:51:13 +0300

    clean

--- monitorZKH.py
+++ monitorZKH.py
@@ -2,25 +2,36 @@ import streamlit as st
 import pandas as pd
 import pyodbc
 
+import pyodbc
+
 def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
+    try:
+        connection_string = (
+            "DRIVER={ODBC Driver 17 for SQL Server};"
+            "SERVER=" + st.secrets["server"] + ";"
+            "DATABASE=" + st.secrets["database"] + ";"
+            "UID=" + st.secrets["username"] + ";"
+            "PWD=" + st.secrets["password"]
+        )
+        conn = pyodbc.connect(connection_string, timeout=5)
+        return conn
+    except pyodbc.OperationalError as e:
+        # Handle the OperationalError exception
+        error_message = str(e)
+        print(f"Ошибка подключения: {error_message}")
+        # You can add additional error handling code here, such as logging or retrying the connection
+        return None
 @st.cache_data(ttl=600)
 
 def run_query(query, params=None):
+    if conn is None:
+        return None  # or raise an exception, or handle the error in some other way
+    
     with conn.cursor() as cur:
         if params:
-          cur.execute(query, params)
+            cur.execute(query, params)
         else:
-          cur.execute(query)
+            cur.execute(query)
         return cur.fetchall()
 def get_regions_dataset() -> pd.DataFrame:  
   rows = run_query("select namespace.NAME, namespace.TYPENAME,ADMHIERARCHY.REGIONCODE FROM ADMHIERARCHY join namespace on ADMHIERARCHY.OBJECTID = namespace.OBJECTID where ADMHIERARCHY.Level = 0 order by ADMHIERARCHY.REGIONCODE")
@@ -30,8 +41,13 @@ def get_city_raion_dataset(region_code) -> pd.DataFrame:
   rows = run_query("select namespace.NAME, namespace.TYPENAME,ADMHIERARCHY.REGIONCODE FROM ADMHIERARCHY join namespace on ADMHIERARCHY.OBJECTID = namespace.OBJECTID where ADMHIERARCHY.Level = 1 and ADMHIERARCHY.REGIONCODE= ? order by ADMHIERARCHY.REGIONCODE", params)
   return rows
 def get_region_df() -> pd.DataFrame:  
+
   regions_dataset = get_regions_dataset()
-  df = pd.DataFrame(dict(zip(['region_name', 'region_type', 'region_code'], zip(*regions_dataset))))
+  if regions_dataset is None:
+      df = pd.DataFrame(columns=['region_name', 'region_type', 'region_code'])
+      st.markdown ("Не удалось подключиться к базе данных.")
+  else:
+      df = pd.DataFrame(dict(zip(['region_name', 'region_type', 'region_code'], zip(*regions_dataset))))
   column_configuration = {
       "region_name": st.column_config.TextColumn(
          "Регион", help="Наименование региона", max_chars=150, width="large"
@@ -39,12 +55,12 @@ def get_region_df() -> pd.DataFrame:
      "region_type": st.column_config.TextColumn(
          "Тип Региона",
          help="Тип Региона",
-         width="small",
+         width="medium"
      ),
      "region_code": st.column_config.TextColumn(
          "Код Региона",
          help="Код Региона",
-         width="medium",
+         width="small"
      ), 
   }
   return df, column_configuration
@@ -67,12 +83,14 @@ def get_city_raion_df(df_region,selected_regions ) -> pd.DataFrame:
      "region_type": st.column_config.TextColumn(
          "Тип Региона",
          help="Тип Региона",
-         width="small",
+         width="medium"
+         
      ),
      "region_code": st.column_config.TextColumn(
          "Код Региона",
          help="Код Региона",
-         width="medium",
+         width="small"
+         
      ), 
     }
   return df_city_raion, column_configuration,region_name,region_type
