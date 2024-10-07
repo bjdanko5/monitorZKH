e6d5fcad533828ebced192b1bebdbe75d55943f1
Boris Zdanko <bjdanko5@gmail.com>
2024-10-07 17:26:35 +0300

    dict

--- widgets/ПоискДома.py
+++ widgets/ПоискДома.py
@@ -1,43 +1,83 @@
 import streamlit as st
+from dataclasses import dataclass
+@dataclass
+class TaggedParams:
+    tag: str
+    params: dict
 try:
     import utils.Поиск_Дома_db as hierarhy_db
 except ImportError as e:
     print("Pressed Reload in Browser...")
 #conn = utils.conn_and_auth_check()
-def ВыборОрганизации(selected_org_container):
-    hierarhy_df = hierarhy_db.get_orgs(0,0)
+def ВыборОрганизации(selected_hierarhy_container,params):
+    params = {"level":0,"parentobjid":0}
+    #selected_hierarhy = (0,0 )
+    if st.session_state.get("tagged_params_dict") is None:
+       st.session_state.tagged_params_dict=[]
+    else:
+        tagged_params_dict = st.session_state.tagged_params_dict   
+    selected_hierarhy_tag = str(params.level)+"_"+str(params.parentobjid)
+    
+    tagged_params_dict[selected_hierarhy_tag] = {"params":params}
+
+    hierarhy_df = hierarhy_db.hierarhy_df(selected_hierarhy.level,selected_hierarhy.parentobjid)
     column_configuration = {
-        "id": st.column_config.NumberColumn(
-            "ИД", 
-            help="ИД", 
+        "objectid": st.column_config.NumberColumn(
+            "objectid",
+            help="objectid",
             width="small"
         ),
         "name": st.column_config.TextColumn(
             "Наименование",
             help="Наименование",
-            width="medium"       
+            width="medium"
         ),
-        "id_role": st.column_config.NumberColumn(
-            "ИД Роли", 
-            help="ИД Роли", 
+        "typename": st.column_config.TextColumn(
+            "Тип",
+            help="Тип",
+            width="medium"
+        ),
+        "level": st.column_config.NumberColumn(
+            "Уровень",
+            help="Уровень",
             width="small"
         ),
-        "role_name": st.column_config.TextColumn(
-            "Наименование Роли",
-            help="Наименование Роли",
-            width="medium"       
+        "parentobjid": st.column_config.NumberColumn(
+            "parentobjid",
+            help="parentobjid",
+            width="small"
         ),
+        "house_objecitid": st.column_config.NumberColumn(
+            "house_objecitid",
+            help="house_objecitid",
+            width="small"
+        ),
+        "housenum": st.column_config.TextColumn(
+            "housenum",
+            help="housenum",
+            width="medium"
+        ),
+        "addnum1": st.column_config.TextColumn(
+            "addnum1",
+            help="addnum1",
+            width="medium"
+        ),
+        "addnum2": st.column_config.TextColumn(
+            "addnum2",
+            help="addnum2",
+            width="medium"
+        )
         
-
     }
+
     def on_select_hierarhy_df():
         if len(st.session_state.event_hierarhy_df.selection.rows) > 0:
-            selected_row_id = st.session_state.event_hierarhy_df.selection.rows[0]
-            
+            selected_row_id = st.session_state.event_hierarhy_df.selection.rows[0]          
             selected_org_id   = hierarhy_df.iloc[selected_row_id]["id"]   
             selected_org_name = hierarhy_df.iloc[selected_row_id]["name"]
             st.session_state.selected_org_id = selected_org_id
             st.session_state.selected_org_name = selected_org_name
+
     if not "selected_org_id" in st.session_state:
         event_hierarhy_df = st.dataframe(
         hierarhy_df, 
@@ -48,14 +88,14 @@ def ВыборОрганизации(selected_org_container):
         selection_mode="single-row",
         key="event_hierarhy_df")
     else:    
-        with selected_org_container:
+        with selected_hierarhy_container:
             if "selected_org_button" in st.session_state:
                 st.empty()
     
         selected_org_button = st.button(
             label = st.session_state.selected_org_name,
             type  ='primary',
-            key   = "selected_org_button"
+            key   = "selected_org_button" +selected_hierarhy_tag
         )   
         if selected_org_button:
             del st.session_state.selected_org_id 
