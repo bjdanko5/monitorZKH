f371ad522c6c2ebe03d6474769c792c98c189971
Boris Zdanko <bjdanko5@gmail.com>
2024-10-08 10:58:58 +0300

    выбор дома

--- utils/Поиск_Дома_db.py
+++ utils/Поиск_Дома_db.py
@@ -5,7 +5,7 @@ from sqlalchemy import text
 def get_hierarchy(level,parentobjid):
     conn = st.session_state["conn"]
     query = """
-            select top 10 
+            select 
                     a.OBJECTID    as objectid,
                     ns.NAME       as name,
                     ns.TYPENAME   as typename,
@@ -37,25 +37,32 @@ def get_hierarchy(level,parentobjid):
 def get_houses(level,parentobjid):
     conn = st.session_state["conn"]
     query = """
-            select top 10 
-                    a.OBJECTID    as objectid,
-                    ns.NAME       as name,
-                    ns.TYPENAME   as typename,
-                    a.Level       as level,
-                    a.parentOBJID as parentobjid,
-                    h.OBJECTID    as house_objectid,
-                    h.HOUSENUM    as housenum,
-                    h.ADDNUM1     as addnum1,
-                    h.ADDNUM2     as addnum2    
-            from ADMHIERARCHY a
-            join namespace as ns
-            on a.OBJECTID = ns.OBJECTID
-            left join HOUSES h
-            on a.OBJECTID = h.OBJECTID  
-            where  a.parentobjid = :parentobjid
-            and a.isactive = 1
+            select
+            street_ns.NAME       as street_name,
+            street_ns.TYPENAME   as street_typename,
+            h.HOUSENUM    as housenum,
+            h.ADDNUM1     as addnum1,
+            h.ADDNUM2     as addnum2,    
+            h.OBJECTID    as house_objectid,
+            hier_street.OBJECTID    as street_objectid,
+            hier_street.parentOBJID as street_parentobjid
+            from ADMHIERARCHY hier_street
+			
+			join namespace as street_ns
+            on hier_street.OBJECTID = street_ns.OBJECTID
+
+			join ADMHIERARCHY hier_house
+			on hier_street.OBJECTID = hier_house.PARENTOBJID
+            
+			join HOUSES h
+            on hier_house.OBJECTID = h.OBJECTID  
+
+            where  hier_street.OBJECTID = :parentobjid
+            and hier_street.isactive = 1
+			and hier_house.isactive = 1
+			and h.isactive = 1
             order by
-            ns.name
+            street_ns.name,h.housenum
             """
     params = {"parentobjid": parentobjid}
     result = conn.execute(text(query), params)
@@ -63,5 +70,5 @@ def get_houses(level,parentobjid):
     if rows: 
         df = pd.DataFrame(rows)
     else:  
-        df = pd.DataFrame(columns=["objectid", "name", "typename", "level", "parentobjid", "house_objectid", "housenum", "addnum1", "addnum2"])
+        df = pd.DataFrame(columns=["street_name", "street_typename", "housenum", "addnum1", "addnum2", "house_objectid", "street_objectid", "street_parentobjid"])
     return df
--- widgets/ПоискДома.py
+++ widgets/ПоискДома.py
@@ -105,3 +105,96 @@ def ПоискДома(selected_hierarchy_container,params):
             st.session_state.tagged_params_dict = {k: v for k, v in st.session_state.tagged_params_dict.items() if int(k) <= int(selected_hierarchy_tag)-1}
             #del tagged_params_dict[selected_hierarchy_tag]
             st.rerun()
+def ВыборДома(selected_hierarchy_container,street_objectid):
+    houses_df = hierarchy_db.get_houses(street_objectid)
+    column_configuration = {
+        "street_name": st.column_config.TextColumn(
+            "Улица",
+            help="Улица",
+            width="medium"
+        ),
+        "street_typename": st.column_config.TextColumn(
+            "Тип улицы",
+            help="Тип улицы",
+            width="medium"
+        ),
+        "housenum": st.column_config.TextColumn(
+            "Номер дома",
+            help="Номер дома",
+            width="medium"
+        ),
+        "addnum1": st.column_config.TextColumn(
+            "Корпус",
+            help="Корпус",
+            width="medium"
+        ),
+        "addnum2": st.column_config.TextColumn(
+            "Строение",
+            help="Строение",
+            width="medium"
+        ),
+        "house_objectid": st.column_config.NumberColumn(
+            "objectid",
+            help="objectid",
+            width="small"
+        ),
+        "street_objectid": st.column_config.NumberColumn(
+            "street_objectid",
+            help="street_objectid",
+            width="small"
+        ),
+        "street_parentobjid": st.column_config.NumberColumn(
+            "street_parentobjid",
+            help="street_parentobjid",
+            width="small"
+        )
+    }
+
+    def on_select_hierarchy_df():
+        if len(st.session_state.event_hierarchy_df.selection.rows) > 0:
+            selected_row_id = st.session_state.event_hierarchy_df.selection.rows[0]  
+        if st.session_state.get("tagged_params_dict") is None:
+            st.session_state.tagged_params_dict={}
+         
+        tagged_params_dict = st.session_state.tagged_params_dict   
+        selected_level   = hierarchy_df.iloc[selected_row_id]["level"]   
+        selected_parentobjid = hierarchy_df.iloc[selected_row_id]["parentobjid"]
+        selected_name= hierarchy_df.iloc[selected_row_id]["name"]
+        selected_typename= hierarchy_df.iloc[selected_row_id]["typename"]
+        selected_objectid = hierarchy_df.iloc[selected_row_id]["objectid"]
+        params = {"level":selected_level,"parentobjid":selected_parentobjid,"objectid":selected_objectid,"name":selected_name,"typename":selected_typename}
+        tagged_params_dict = st.session_state.tagged_params_dict   
+        selected_hierarchy_tag = str(selected_level)          
+        tagged_params_dict[selected_hierarchy_tag] = {"params":params}       
+        st.session_state.tagged_params_dict = tagged_params_dict   
+
+    selected_hierarchy_tag = str(params["level"])
+    #
+    if st.session_state.get("tagged_params_dict") is None:
+            st.session_state.tagged_params_dict={}
+    tagged_params_dict = st.session_state.tagged_params_dict        
+    if not selected_hierarchy_tag in tagged_params_dict:
+        event_hierarchy_df = st.dataframe(
+        hierarchy_df, 
+        column_config=column_configuration,
+        use_container_width=True,
+        hide_index=True,
+        on_select=on_select_hierarchy_df,
+        selection_mode="single-row",
+        key="event_hierarchy_df")
+    else:    
+        with selected_hierarchy_container:
+            if "selected_org_button"+selected_hierarchy_tag in st.session_state:
+                st.empty()
+        selected_hierarchy_button = st.button(
+            label = str(st.session_state.tagged_params_dict[selected_hierarchy_tag]["params"]["typename"]+" "+
+                        st.session_state.tagged_params_dict[selected_hierarchy_tag]["params"]["name"]
+                        )
+            ,
+            type  ='primary',
+            key   = "selected_org_button" +selected_hierarchy_tag
+        )   
+        if selected_hierarchy_button:
+            st.session_state.tagged_params_dict = {k: v for k, v in st.session_state.tagged_params_dict.items() if int(k) <= int(selected_hierarchy_tag)-1}
+            #del tagged_params_dict[selected_hierarchy_tag]
+            st.rerun()
